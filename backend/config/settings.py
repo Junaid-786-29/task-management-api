75b305ca214dbde64e85a25bff48034cf766b43b
Junaid Khan <junaidjk78629@gmail.com>
2026-08-31 12:05:49 +0530

    Configure CORS for Vercel Frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -179,6 +179,7 @@ SIMPLE_JWT = {
 # Origins for frontend
 
 CORS_ALLOWED_ORIGINS = [
+    "https://task-management-api-gamma-lyart.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
